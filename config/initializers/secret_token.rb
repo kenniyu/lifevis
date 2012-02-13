67765c274a96ffb66e5b88358bbfa39b34875730
ken yu <kenniyu@gmail.com>
2012-02-13 03:29:57 -0800

    lifevis

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifevis::Application.config.secret_token = '4e8f106a98a9a0bfa160b941a69697c3173d13151d74b64a04ecb198aaab737a7e0a4d8070d4ad588c9fc3e278a5f2ebba6bda981da2782622d4df7870e975ee'
